eca6473e75527040cebee58a9f031f7db0ad7ab8
Stef Spears <spears.sj87@gmail.com>
2019-10-10 02:49:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,13 @@
 class CashRegister
   
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :title, :price
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
   end
+  
+  def add_item
+  end 
   
 end 
