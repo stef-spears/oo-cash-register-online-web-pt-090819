92f7e20d7ad7175e2cbfc5feca0bbd8a6d94e016
Stef Spears <spears.sj87@gmail.com>
2019-10-10 03:05:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,9 +21,10 @@ class CashRegister
   
   def apply_discount
     if @discount.positive?
-      @total - (@total * (@discount.to_f/100))
+      @total -= (@total * (@discount.to_f/100))
     end
-    binding.pry
+    total.to_i
+    #binding.pry
   end 
   
 end 
